556b95d6c9ea851a8018e1577f10f825b5f564a6
aucuba <aoki.11235813@ezweb.ne.jp>
2018-09-30 22:52:10 +0900

    add funciton

--- app.js
+++ app.js
@@ -4,33 +4,62 @@ const MyComponent = {
         '<button class="delete"></button>' +
         '{{msg.text}}</div>'
 }
-// 登録する
-// Vue.component('my-component', MyComponent)
-// root インスタンスを作成する
+
 const timeLine = new Vue({
     el: '#example',
     data:function(){
-        return {message:[
-                // {id:1,text:'yo'},
-                // // {id:2,text:'yo bro'}
-            ]}
+        return {message:[]}
     },
-
     components: {
         'my-component': MyComponent
     },
+    method:{
+    }
 })
 
 const form = new Vue({
     el: '#msgForm',
     data: {
-        message: 'fafa'
+        message: 'text'
     },
     methods: {
         async submit () {
             await console.log(this.message);
             await timeLine.message.push({id:timeLine.message.length+1, text:this.message});
             this.message='';
+            document.querySelectorAll('.delete').forEach((elm) => {
+                elm.addEventListener('click', (ev) => {
+                    const target = ev.target.closest('.notification');
+                    if (target) {
+                        target.parentNode.removeChild(target);
+                    }
+                });
+            });
+        }
+    },
+});
+export default {
+    data() {
+        return {
+            items: [],
+            json: ''
+        }
+    },
+    mounted() {
+        this.items = JSON.parse(localStorage.getItem('items')) || [];
+    },
+    methods: {
+        addItem() {
+            this.items.push('item_' + this.items.length);
+            this.setItems();
+        },
+        deleteAllItems() {
+            this.items = [];
+            this.setItems();
+        },
+        setItems() {
+            localStorage.setItem('items', JSON.stringify(this.items));
         }
     }
-});
+}
+
