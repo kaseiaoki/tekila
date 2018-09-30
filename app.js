72f73a76b74e4f4987eeef933cbfa4672f27c483
aucuba <aoki.11235813@ezweb.ne.jp>
2018-09-30 23:29:22 +0900

    use loacal S

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const MyComponent = {
 const timeLine = new Vue({
     el: '#example',
     data:function(){
-        return {message:[]}
+        let isItem=JSON.parse(localStorage.getItem('items'));
+        let messageArray =[];
+        for(let item in isItem){
+           messageArray.push({id:item, text:isItem[item]});
+        }
+        return {message:messageArray}
     },
     components: {
         'my-component': MyComponent
@@ -20,12 +25,29 @@ const timeLine = new Vue({
 const form = new Vue({
     el: '#msgForm',
     data: {
-        message: 'text'
+        message: 'text',
+        items: [],
+        json:''
+
+    },
+    mounted() {
+        this.items = JSON.parse(localStorage.getItem('items')) || [];
     },
     methods: {
-        async submit () {
+    addItem() {
+        this.items.push(this.message);
+        this.setItems();
+    },
+    deleteAllItems() {
+        this.items = [];
+        this.setItems();
+    },
+    setItems() {
+        localStorage.setItem('items', JSON.stringify(this.items));
+    }, async submit () {
             await console.log(this.message);
             await timeLine.message.push({id:timeLine.message.length+1, text:this.message});
+            await this.addItem();
             this.message='';
             document.querySelectorAll('.delete').forEach((elm) => {
                 elm.addEventListener('click', (ev) => {
@@ -38,28 +60,5 @@ const form = new Vue({
         }
     },
 });
-export default {
-    data() {
-        return {
-            items: [],
-            json: ''
-        }
-    },
-    mounted() {
-        this.items = JSON.parse(localStorage.getItem('items')) || [];
-    },
-    methods: {
-        addItem() {
-            this.items.push('item_' + this.items.length);
-            this.setItems();
-        },
-        deleteAllItems() {
-            this.items = [];
-            this.setItems();
-        },
-        setItems() {
-            localStorage.setItem('items', JSON.stringify(this.items));
-        }
-    }
-}
+
 
